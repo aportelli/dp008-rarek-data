2010746e5fe356eaa5da702cbed20fae5c00a7db
Antonin Portelli <antonin.portelli@me.com>
2022-03-14 18:49:12 +0000

    safer checksum for rclone

--- bootstrap.sh
+++ bootstrap.sh
@@ -54,10 +54,16 @@ case "$OS_type" in
 esac
 
 # download and unzip
+sha1='9009ac520214b6b4ce781c819a4e2b3d43dbd74a'
+
 rclone_version='1.57.0'
 rclone_zip="rclone-v${rclone_version}-${OS}-${OS_type}.zip"
 curl -OfsS "https://downloads.rclone.org/v${rclone_version}/${rclone_zip}"
 curl -s "https://downloads.rclone.org/v${rclone_version}/SHA1SUMS" | grep ${rclone_zip} > SHA1SUM
+if [ ! ${sha1} = $(awk '{print $1}' SHA1SUM) ]; then
+  echo "error: SHA1 checksum from rclone website does not match expected checksum" 1>&2
+  exit 1
+fi
 sha1sum -c SHA1SUM
 unzip ${rclone_zip}
 mkdir -p bin
