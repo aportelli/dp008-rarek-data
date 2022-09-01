2031578658a3c75b4781235cb936d7dd5606b46d
Raoul Hodgson <raoul.hodgson@ed.ac.uk>
2022-09-01 12:48:13 +0100

    Fixed OSX specific SHA1 hash check

--- bootstrap.sh
+++ bootstrap.sh
@@ -54,19 +54,18 @@ case "$OS_type" in
 esac
 
 # download and unzip
-sha1='9009ac520214b6b4ce781c819a4e2b3d43dbd74a'
-
 rclone_version='1.57.0'
 rclone_zip="rclone-v${rclone_version}-${OS}-${OS_type}.zip"
 curl -OfsS "https://downloads.rclone.org/v${rclone_version}/${rclone_zip}"
-curl -s "https://downloads.rclone.org/v${rclone_version}/SHA1SUMS" | grep ${rclone_zip} > SHA1SUM
-if [ ! ${sha1} = $(awk '{print $1}' SHA1SUM) ]; then
-  echo "error: SHA1 checksum from rclone website does not match expected checksum" 1>&2
-  exit 1
+curl -s "https://downloads.rclone.org/v${rclone_version}/SHA1SUMS" > SHA1SUMS_tmp
+if [ $(cmp --silent SHA1SUMS SHA1SUMS_tmp) -e 0 ]; then
+  echo "error: SHA1 checksums from rclone website do not match expected checksums" 1>&2
+	exit 1
 fi
-sha1sum -c SHA1SUM
+grep ${rclone_zip} SHA1SUMS > SHA1SUMS_tmp
+sha1sum -c SHA1SUMS_tmp
 unzip ${rclone_zip}
 mkdir -p bin
 mv ${rclone_zip//.zip}/rclone bin/
 PATH="$(pwd -P)/bin:${PATH}"
-rm -rf ${rclone_zip} ${rclone_zip//.zip} SHA1SUM
+rm -rf ${rclone_zip} ${rclone_zip//.zip} SHA1SUMS_tmp
